940b1ca92d2541fd0444a3ce789e6593b99ef98e
Polyotu <55525642+Polyotu@users.noreply.github.com>
2022-01-05 15:53:58 +0900

    additionalとoptionalを逆にした

--- register.js
+++ register.js
@@ -23,12 +23,14 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 
 //準備
 const { Client, ClientApplication } = require("discord.js");
-const { add } = require('nodemon/lib/rules');
 const client = new Client({
     intents: 0,
 });
+
+//bot動作トークン
 client.token = tokens.bot;
 
+//******************************************************************************************** */
 //登録用の関数．client,登録するコマンドのリスト，対象となる鯖IDがセット
 //鯖IDがnullだとdiscord側でglobalコマンドとして登録される
 async function register(client, commands, guildID) {
@@ -40,19 +42,23 @@ async function register(client, commands, guildID) {
     // console.log(commands);
     return guild.commands.set(commands);
 }
+//******************************************************************************************** */
 
-const allOptionalCommands = [];
+//コマンドをリスト化する
+//base：globalコマンドに相当．更新に時間がかかるが全サーバに自動実装．
+//optional：guildコマンドだが，baseの拡張コマンド相当．
+//additional：ほとんどがaddコマンドでの追加対象．デフォではサーバに実装されていない．
 const additionalCommands = [];
+const optionalCommands = [];
 const baseCommands = [];
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    // commands[file.replace(".js","")] = command;
     if (command.attr == "base") {
         baseCommands[baseCommands.length] = command.data;
     } else if (command.attr == "additional") {
-        additionalCommands[additionalCommands.length] = command.data;
+        optionalCommands[optionalCommands.length] = command.data;
     } else if (command.attr == "option") {
-        allOptionalCommands[allOptionalCommands.length] = command.data;
+        additionalCommands[additionalCommands.length] = command.data;
     }
 }
 
@@ -60,19 +66,30 @@ async function main() {
     client.application = new ClientApplication(client, {});
     await client.application.fetch();
 
+    //鯖IDをnullにしてregister->globalコマンドの登録
     await register(client, baseCommands, null);
     console.log(baseCommands);
     console.log(`global : registration succeed!`);
 
+    //各鯖に登録するコマンドのリスト
     let commandList;
+
+    //commandListを埋める
+    //(commands.jsonに登録されている)サーバの数だけforループ
     for (let i = 0; i < registerSet.length; i++) {
-        commandList = additionalCommands.concat();
+
+        //optional系はbaseの拡張であるため全サーバに実装．
+        commandList = optionalCommands.concat();
+
+        //各鯖の.registerCommands部分をcommandListに登録していく
         for (let j = 0; j < registerSet[i].registerCommands.length; j++) {
-            let pos = allOptionalCommands.findIndex((c) => c.name === registerSet[i].registerCommands[j]);
+            let pos = additionalCommands.findIndex((c) => c.name === registerSet[i].registerCommands[j]);
             if (pos >= 0) {
-                commandList.push(allOptionalCommands[pos]);
+                commandList.push(additionalCommands[pos]);
             }
         }
+
+        //commandListが完成したのでguildコマンドとして登録
         await register(client, commandList, registerSet[i].id);
         console.log(commandList);
         console.log(`${registerSet[i].name} : registration succeed!`);
