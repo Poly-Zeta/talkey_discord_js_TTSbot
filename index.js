2917c935015de10ff96e21ec6823c8ac3da43d9a
Polyotu <55525642+Polyotu@users.noreply.github.com>
2022-01-18 14:11:02 +0900

    タイマー切断時にステータス表示を更新

--- index.js
+++ index.js
@@ -342,6 +342,7 @@ client.on('ready', () => {
             const guild = client.guilds.cache.get(elem);
             guild.systemChannel.send('一定時間読み上げ指示が無かったため，切断しました．');
         }
+        client.user.setActivity(statusMessageGen(getVoiceConnections().size, client.guilds.cache.size), { type: 'LISTENING' });
     });
 });
 
