aeafdc27721b55a49fd3c758cd20fda9816eef24
Polyotu <55525642+Polyotu@users.noreply.github.com>
2022-03-27 07:22:39 +0900

    お試し

--- commands/tomb.js
+++ commands/tomb.js
@@ -5,36 +5,47 @@ module.exports = {
         description: "某AAを表示する",
         options: [
             {
-                type: "SUB_COMMAND",
-                name: "default",
-                description: "†┏┛墓┗┓†",
-            },
-            {
-                type: "SUB_COMMAND",
-                name: "insert",
+                type:"STRING",
+                name:"insert",
                 description: "表示する内容を一部差し換える",
-                options: [
-                    {
-                        type: "STRING",
-                        name: "text",
-                        description: "差し替え先",
-                        required: true
-                    }
-                ]
-            },
+                required:false,
+            }
+            // {
+            //     type: "SUB_COMMAND",
+            //     name: "default",
+            //     description: "†┏┛墓┗┓†",
+            // },
+            // {
+            //     type: "SUB_COMMAND",
+            //     name: "insert",
+            //     description: "表示する内容を一部差し換える",
+            //     options: [
+            //         {
+            //             type: "STRING",
+            //             name: "text",
+            //             description: "差し替え先",
+            //             required: true
+            //         }
+            //     ]
+            // },
         ]
     },
     async execute(interaction) {
-        const subCommand = interaction.options.getSubcommand(false);
-        console.log(subCommand);
-        if (subCommand == "default") {
-            const reply = "†┏┛墓┗┓†";
-            return interaction.reply(reply);
-        } else if (subCommand == "insert") {
-            const reply = `†┏┛${interaction.options.get("text").value}┗┓†`;
-            return interaction.reply(reply);
-        } else {
-            return interaction.reply("エラー");
-        }
+        const cmdOption=interaction.options.get("insert",false);
+        const insertText=cmdOption.value ?? "墓";
+        const reply = `†┏┛${insertText}┗┓†`;
+        return interaction.reply(reply);
+
+        // const subCommand = interaction.options.getSubcommand(false);
+        // console.log(subCommand);
+        // if (subCommand == "default") {
+        //     const reply = "†┏┛墓┗┓†";
+        //     return interaction.reply(reply);
+        // } else if (subCommand == "insert") {
+        //     const reply = `†┏┛${interaction.options.get("text").value}┗┓†`;
+        //     return interaction.reply(reply);
+        // } else {
+        //     return interaction.reply("エラー");
+        // }
     }
 }
