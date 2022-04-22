9d054e29ff846f9fdbc2f7cddadc9c8e545fdce2
Polyotu <55525642+Polyotu@users.noreply.github.com>
2022-04-22 13:11:51 +0900

    修正

--- commands/md.js
+++ commands/md.js
@@ -12,37 +12,37 @@ module.exports = {
             .addFields(
                 {
                     name: "斜体",
-                    value: "\\*または\\_で文章を囲むと_斜体_にできます．",
+                    value: "\*または\_で文章を囲むと_italic_(斜体)にできます．",
                     inline: false
                 },
                 {
                     name: "太字",
-                    value: "\\**で文章を囲むと**太字**にできます．",
+                    value: "\*\*で文章を囲むと**太字**にできます．",
                     inline: false
                 },
                 {
                     name: "下線",
-                    value: "\\__で文章を囲むと__下線__を引けます．",
+                    value: "\_\_で文章を囲むと__下線__を引けます．",
                     inline: false
                 },
                 {
                     name: "打消し線",
-                    value: "\\~~で文章を囲むと~~打消し線~~を引けます．",
+                    value: "\~\~で文章を囲むと~~打消し線~~を引けます．",
                     inline: false
                 },
                 {
                     name: "ネタばれ隠し",
-                    value: "\\||で文章を囲むと||ネタばれ||を隠せます．",
+                    value: "\|\|で文章を囲むと||ネタばれ||を隠せます．",
                     inline: false
                 },
                 {
                     name: "インラインコードブロック",
-                    value: "\\`で文章を囲むと`inline code block`になります．",
+                    value: "\`で文章を囲むと`inline code block`になります．",
                     inline: false
                 },
                 {
                     name: "コードブロック",
-                    value: "\\```で文章を囲むと```print('code block')```になります．",
+                    value: "\`\`\`で文章を囲むと```print('code block')```になります．",
                     inline: false
                 },
                 {
@@ -52,7 +52,7 @@ module.exports = {
                 },
                 {
                     name: "無効化",
-                    value: "`\\`の\\**後\\**に入力された装飾は\\__無効化\\__されます．",
+                    value: "\の**後**に入力された装飾は__無効化__されます．",
                     inline: false
                 },
             )
