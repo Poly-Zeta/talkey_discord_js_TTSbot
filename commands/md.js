c67a0dea1dc16800b68b04c60a203acfe44ffa5a
Polyotu <55525642+Polyotu@users.noreply.github.com>
2022-04-22 13:14:32 +0900

    修正

--- commands/md.js
+++ commands/md.js
@@ -17,32 +17,32 @@ module.exports = {
                 },
                 {
                     name: "太字",
-                    value: "\*\*で文章を囲むと**太字**にできます．",
+                    value: "\**で文章を囲むと**太字**にできます．",
                     inline: false
                 },
                 {
                     name: "下線",
-                    value: "\_\_で文章を囲むと__下線__を引けます．",
+                    value: "\__で文章を囲むと__下線__を引けます．",
                     inline: false
                 },
                 {
                     name: "打消し線",
-                    value: "\~\~で文章を囲むと~~打消し線~~を引けます．",
+                    value: "\~~で文章を囲むと~~打消し線~~を引けます．",
                     inline: false
                 },
                 {
                     name: "ネタばれ隠し",
-                    value: "\|\|で文章を囲むと||ネタばれ||を隠せます．",
+                    value: "\||で文章を囲むと||ネタばれ||を隠せます．",
                     inline: false
                 },
                 {
                     name: "インラインコードブロック",
-                    value: "\`で文章を囲むと`inline code block`になります．",
+                    value: "\\`で文章を囲むと`inline code block`になります．",
                     inline: false
                 },
                 {
                     name: "コードブロック",
-                    value: "\`\`\`で文章を囲むと```print('code block')```になります．",
+                    value: "\```で文章を囲むと```print('code block')```になります．",
                     inline: false
                 },
                 {
