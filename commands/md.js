132f78f556294d42372e1d4bb528ac105d70053b
Polyotu <55525642+Polyotu@users.noreply.github.com>
2022-04-22 12:54:44 +0900

    修正

--- commands/md.js
+++ commands/md.js
@@ -12,47 +12,47 @@ module.exports = {
             .addFields(
                 {
                     name: "斜体",
-                    value: "\*または\_で文章を囲むと_斜体_にできます．",
+                    value: "\*または_\で文章を囲むと_斜体_にできます．",
                     inline: false
                 },
                 {
                     name: "太字",
-                    value: "\*\*で文章を囲むと**太字**にできます．",
+                    value: "\**\で文章を囲むと**太字**にできます．",
                     inline: false
                 },
                 {
                     name: "下線",
-                    value: "\_\_で文章を囲むと__下線__を引けます．",
+                    value: "\__\で文章を囲むと__下線__を引けます．",
                     inline: false
                 },
                 {
                     name: "打消し線",
-                    value: "\~\~で文章を囲むと~~打消し線~~を引けます．",
+                    value: "\~~\で文章を囲むと~~打消し線~~を引けます．",
                     inline: false
                 },
                 {
                     name: "ネタばれ隠し",
-                    value: "\|\|で文章を囲むと||ネタばれ||を隠せます．",
+                    value: "\||\で文章を囲むと||ネタばれ||を隠せます．",
                     inline: false
                 },
                 {
                     name: "インラインコードブロック",
-                    value: "\`で文章を囲むと`inline code block`になります．",
+                    value: "\`\で文章を囲むと`inline code block`になります．",
                     inline: false
                 },
                 {
                     name: "コードブロック",
-                    value: "\`\`\`で文章を囲むと```print('code block')```になります．",
+                    value: "\```\で文章を囲むと```print('code block')```になります．",
                     inline: false
                 },
                 {
                     name: "引用",
-                    value: "文章の前に`> `または`>>>`を入力すると，文章を引用できます．",
+                    value: "文章の前に`> `または`>>>`を入力すると，>>>文章を引用できます．",
                     inline: false
                 },
                 {
                     name: "無効化",
-                    value: "ここまでの装飾は，各記号の前に`\\`を入力することで無効化できます．",
+                    value: "バックスラッシュで囲まれた範囲の装飾は無効化されます．",
                     inline: false
                 },
             )
