7fe6ded06be3336b00d6801599303987670aed61
Polyotu <55525642+Polyotu@users.noreply.github.com>
2022-04-24 05:08:30 +0900

    クラッシュしなかったのでもうちょっと強いやつに変える

--- index.js
+++ index.js
@@ -416,7 +416,7 @@ client.on('ready', () => {
     //3日以上連続稼働した場合の定期再起動
     cron.schedule('0 12 */3 * *', () => {
         console.log("perform periodic reboots.");
-        client.channels.cache.get(tokens.bootNotifyChannel).send('定期再起動を実行．');
+        await client.channels.cache.get(tokens.bootNotifyChannel).send('定期再起動を実行．');
         statConfig.reboot += 1;
         fs.writeFileSync(
             path.resolve(__dirname, absolutePath.stat),
