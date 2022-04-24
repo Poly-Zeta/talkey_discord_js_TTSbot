6f0dc43f24a2d12589b3acc4995eb7b39e95a615
Polyotu <55525642+Polyotu@users.noreply.github.com>
2022-04-24 21:18:31 +0900

    throw Errorさせてみる

--- commands/ping.js
+++ commands/ping.js
@@ -11,7 +11,8 @@ module.exports = {
             "",
             `gateway: ${interaction.client.ws.ping}ms`,
         ];
-        await interああああああああああああああaction.reply({ content: msg.join("\n") });
+        throw new Error("test err");
+        await interaction.reply({ content: msg.join("\n") });
         await interaction.editReply([...msg, `往復: ${Date.now() - now}ms`].join("\n"));
         return;
     }
