725874fccfdb5a21ea406d8ec80db5054f4768a6
Polyotu <55525642+Polyotu@users.noreply.github.com>
2022-05-26 22:16:35 +0900

    vc自動ログアウトメッセージのチェック_追記12

--- index.js
+++ index.js
@@ -411,20 +411,7 @@ client.on('ready', () => {
         const idList = scanQueueMap(now);
         // console.log(idList);
         for (const elem of idList) {
-            console.log(`elem:${elem}`);
-
-            //これ本当にawait無しで大丈夫なのかわからん
-            const botVcData = getGuildMap(elem).then(()=>{
-                console.log(botVcData);
-                client.channels.cache.get(botVcData.textChannelId).send('一定時間読み上げ指示が無かったため，切断しました．')
-            }).then(()=>{
-                console.log(`textChannelId:${textChannelId}`);
-            });
-            
-            const botConnection = getVoiceConnection(elem);
-            botConnection.destroy().then(()=>{
-                deleteGuildToMap(elem);
-            });
+            vcAutoDisconnect(elem);
         }
         client.user.setActivity(statusMessageGen(getVoiceConnections().size, client.guilds.cache.size), { type: 'LISTENING' });
     });
@@ -461,6 +448,17 @@ client.on('ready', () => {
         });
     });
 });
+
+async function vcAutoDisconnect(elem){
+    console.log(`elem:${elem}`);
+    const botVcData = await getGuildMap(elem)
+    console.log(botVcData);
+    await deleteGuildToMap(elem);
+    await client.channels.cache.get(botVcData.textChannelId).send('一定時間読み上げ指示が無かったため，切断しました．')
+    console.log(`textChannelId:${textChannelId}`);
+    const botConnection = getVoiceConnection(elem);
+    botConnection.destroy();
+}
 
 async function onMessage(message) {
     //token削除
