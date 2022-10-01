d1eb5ed28a1225a6fece54a340cb7e93dc22eaf2
Polyotu <55525642+Polyotu@users.noreply.github.com>
2022-10-01 18:46:57 +0900

    ギアパワー調整コマンド実装

--- commands/ikanogearpower.js
+++ commands/ikanogearpower.js
@@ -46,10 +46,10 @@ module.exports = {
         }
 
         const msgAry=readTxt.split(/[.,]/);
-        console.log(`msgAry:${msgAry}`);
+        // console.log(`msgAry:${msgAry}`);
         gearPowerBig=parseInt(msgAry[0], 10);
         gearPowerSmall=parseInt(msgAry[1], 10);
-        console.log(`big:${gearPowerBig},small:${gearPowerSmall}`);
+        // console.log(`big:${gearPowerBig},small:${gearPowerSmall}`);
 
         if(option.value=="effect"){
             if(gearPowerBig*10+gearPowerSmall>57){
@@ -59,19 +59,19 @@ module.exports = {
             let pow=gearPowerBig*10+gearPowerSmall;
             let big=0;
             let small=0;
-            console.log(`pow:${pow},big:${big},small:${small}`);
+            // console.log(`pow:${pow},big:${big},small:${small}`);
             if(pow%10==0){
                 big=pow/10;
-                console.log(`a:pow:${pow},big:${big},small:${small}`);
+                // console.log(`a:pow:${pow},big:${big},small:${small}`);
             }else{
                 if((pow%10)%3==0){
                     big=~~(pow/10);
                     small=(pow%10)/3;
-                    console.log(`c:pow:${pow},big:${big},small:${small}`);
+                    // console.log(`c:pow:${pow},big:${big},small:${small}`);
                 }else{
                     if(pow%3==0){
                         small=pow/3;
-                        console.log(`b:pow:${pow},big:${big},small:${small}`);
+                        // console.log(`b:pow:${pow},big:${big},small:${small}`);
                     }
                     else{
                         big=-1;
@@ -79,7 +79,7 @@ module.exports = {
                 }
             }
             if(big<0){
-                reply="解が見つかりませんでした．";
+                reply=`5.7表記[${readTxt}]について，3,9表記の場合の解が見つかりませんでした．`;
             }else{
                 reply=`5.7表記[${readTxt}]は，3,9表記で${big},${small}です`;
             }
