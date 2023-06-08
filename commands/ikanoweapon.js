9ee9a9363bbaf42463dc2970eaefc1d7b43c04d5
Polyotu <55525642+Polyotu@users.noreply.github.com>
2023-06-09 02:38:55 +0900

    スプラ3だけにしてみる

--- commands/ikanoweapon.js
+++ commands/ikanoweapon.js
@@ -25,12 +25,13 @@ module.exports = {
     data: {
         name: "ikanoweapon",
         description: "スプラのブキランダマイザ",
-        options: [
+        options: 
+        //[
             {
-                type: 1,//"SUB_COMMAND",
-                name: "spl3",
-                description: "スプラ3のブキセットから選出",
-                options: {
+                // type: 1,//"SUB_COMMAND",
+                // name: "spl3",
+                // description: "スプラ3のブキセットから選出",
+                // options: {
                     type: 3,//"STRING",
                     name: "filter3",
                     description: "ブキカテゴリのフィルタ",
@@ -85,66 +86,66 @@ module.exports = {
                         },
                     ]
                 }
-            },
-            {
-                type: 1,//"SUB_COMMAND",
-                name: "spl2",
-                description: "スプラ2のブキセットから選出",
-                options: {
-                    type: 3,//"STRING",
-                    name: "filter2",
-                    description: "ブキカテゴリのフィルタ",
-                    choices: [
-                        {
-                            name: "シューター",
-                            value: "shooter",
-                        },
-                        {
-                            name: "ブラスター",
-                            value: "blaster",
-                        },
-                        {
-                            name: "リールガン",
-                            value: "reelgun",
-                        },
-                        {
-                            name: "ローラー",
-                            value: "roller",
-                        },
-                        {
-                            name: "フデ",
-                            value: "brush",
-                        },
-                        {
-                            name: "チャージャー",
-                            value: "charger",
-                        },
-                        {
-                            name: "マニューバ―",
-                            value: "maneuver",
-                        },
-                        {
-                            name: "スピナー",
-                            value: "splatling",
-                        },
-                        {
-                            name: "スロッシャー",
-                            value: "slosher",
-                        },
-                        {
-                            name: "シェルター",
-                            value: "brella",
-                        },
-                    ]
-                }
-            }
-        ]
+            // },
+            // {
+            //     type: 1,//"SUB_COMMAND",
+            //     name: "spl2",
+            //     description: "スプラ2のブキセットから選出",
+            //     options: {
+            //         type: 3,//"STRING",
+            //         name: "filter2",
+            //         description: "ブキカテゴリのフィルタ",
+            //         choices: [
+            //             {
+            //                 name: "シューター",
+            //                 value: "shooter",
+            //             },
+            //             {
+            //                 name: "ブラスター",
+            //                 value: "blaster",
+            //             },
+            //             {
+            //                 name: "リールガン",
+            //                 value: "reelgun",
+            //             },
+            //             {
+            //                 name: "ローラー",
+            //                 value: "roller",
+            //             },
+            //             {
+            //                 name: "フデ",
+            //                 value: "brush",
+            //             },
+            //             {
+            //                 name: "チャージャー",
+            //                 value: "charger",
+            //             },
+            //             {
+            //                 name: "マニューバ―",
+            //                 value: "maneuver",
+            //             },
+            //             {
+            //                 name: "スピナー",
+            //                 value: "splatling",
+            //             },
+            //             {
+            //                 name: "スロッシャー",
+            //                 value: "slosher",
+            //             },
+            //             {
+            //                 name: "シェルター",
+            //                 value: "brella",
+            //             },
+            //         ]
+            //     }
+            // }
+        //]
     },
     async execute(interaction) {
-        const subCommand = interaction.options.getSubcommand(false);
-        console.log(subCommand);
+        // const subCommand = interaction.options.getSubcommand(false);
+        // console.log(subCommand);
 
-        if (subCommand === "spl3") {
+        // if (subCommand === "spl3") {
             const option = interaction.options.get("filter3", false);
             let reply = "";
             if (option == null) {
@@ -161,24 +162,24 @@ module.exports = {
             }
             // return interaction.reply(reply);
             return interaction.editReply(reply);
-        }
-        else if (subCommand === "spl2") {
-            const option = interaction.options.get("filter2", false);
-            let reply = "";
-            if (option == null) {
-                const ans = ndnDiceRoll(1, weaponData2.length);
-                reply = weaponData2[ans-1].name.ja_JP;
-            } else {
-                var filterdWeapons = weaponData2.filter(function (item, index) {
-                    if (item.type.key == option.value) return true;
-                });
-                const ans = ndnDiceRoll(1, filterdWeapons.length);
-                // console.log(filterdWeapons.length);
-                // console.log(ans);
-                reply = `${option.value}->${filterdWeapons[ans - 1].name.ja_JP}`;
-            }
-            // return interaction.reply(reply);
-            return interaction.editReply(reply);
-        };
+    //     }
+    //     else if (subCommand === "spl2") {
+    //         const option = interaction.options.get("filter2", false);
+    //         let reply = "";
+    //         if (option == null) {
+    //             const ans = ndnDiceRoll(1, weaponData2.length);
+    //             reply = weaponData2[ans-1].name.ja_JP;
+    //         } else {
+    //             var filterdWeapons = weaponData2.filter(function (item, index) {
+    //                 if (item.type.key == option.value) return true;
+    //             });
+    //             const ans = ndnDiceRoll(1, filterdWeapons.length);
+    //             // console.log(filterdWeapons.length);
+    //             // console.log(ans);
+    //             reply = `${option.value}->${filterdWeapons[ans - 1].name.ja_JP}`;
+    //         }
+    //         // return interaction.reply(reply);
+    //         return interaction.editReply(reply);
+    //     };
     }
 }
