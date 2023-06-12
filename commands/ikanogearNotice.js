b5617d0bfe5861267f48b1b9ad9d9358a640b11c
Polyotu <55525642+Polyotu@users.noreply.github.com>
2023-06-13 03:07:48 +0900

    機能を省略

--- commands/ikanogearNotice.js
+++ commands/ikanogearNotice.js
@@ -23,37 +23,11 @@ module.exports = {
     data: {
         name: "ikanogearnotice",
         description: "ギア配信通知リストの操作",
-        options: [
-            {
-                type: 3,//"STRING",
-                name: "op",
-                description: "操作を指定",
-                choices: [
-                    {
-                        name: "add(ギア通知リストに新規データを登録)",
-                        value: "add",
-                    },
-                    {
-                        name: "delete(ギア通知リストからデータを削除)",
-                        value: "delete",
-                    }
-                ],
-                required: true
-            }
-        ]
     },
     async execute(interaction) {
-        const option = interaction.options.get("op", false);
-        console.log(option);
         let reply = "";
-
-        if(option.value==="add"){
-            reply=`次のGoogleFormリンクから，通知してほしいギアの登録を行ってください．\n${formsURL}${interaction.member.id}`;
-            interaction.editReply(`${interaction.member.displayName}さんがaddを実行しました．`);
-            return interaction.followUp({content:reply,ephemeral:true});
-        }else{
-            reply="工事中";
-            return interaction.editReply(reply);
-        }
+        reply=`次のGoogleFormリンクから，通知してほしいギアの登録を行ってください．\n${formsURL}${interaction.member.id}`;
+        interaction.editReply(`${interaction.member.displayName}さんがaddを実行しました．`);
+        return interaction.followUp({content:reply,ephemeral:true});
     }
 }
