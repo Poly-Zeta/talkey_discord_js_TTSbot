26b9ab1b5cc2503bde5cbaf09866b0ca353e6ab5
Polyotu <55525642+Polyotu@users.noreply.github.com>
2023-09-01 07:31:08 +0900

    ミス修正

--- functions/llmMap.js
+++ functions/llmMap.js
@@ -4,14 +4,14 @@ const {getResponseofTranslateAPI,getResponseofLlamaAPI} = require('../functions/
 //https://zenn.dev/s7/articles/86511eb5089fb6c05599
 //************************************************************************************************************************ */
 
-const llmQueue = new Map();
+const llmQueue = [];
 
 async function addLlamaQueue(guildId, nickname, readTxt, uid,textChannel,botConnection) {
     const queue = llmQueue;
     queue.push({guildId, nickname, readTxt, uid,textChannel,botConnection});
-    if (queue.length == 0) {
+    if (queue.length != 0) {
+        console.log(llmQueue);
         processLlamaQueue();
-        // console.log(llmQueue);
     }
     return;
 };
@@ -23,20 +23,20 @@ async function processLlamaQueue() {
     }
     
     //英訳
-    queue.readTxt=getResponseofTranslateAPI(queue.readTxt,"ja","en");
+    queue[0].readTxt=getResponseofTranslateAPI(queue[0].readTxt,"ja","en");
 
     //llama
 
     //和訳
-    queue.readTxt=getResponseofTranslateAPI(queue.readTxt,"en","ja");
+    queue[0].readTxt=getResponseofTranslateAPI(queue[0].readTxt,"en","ja");
 
     //音声再生にスタック<-botConnectionに応じた動作が必要かも
 
     //該当テキストチャットにメッセージ送信
-    queue.textChannel.send(queue.readTxt);
+    queue[0].textChannel.send(queue[0].readTxt);
 
     queue.shift();
-    processLlamaQueue(guildId);
+    processLlamaQueue();
 };
 
 module.exports = {
