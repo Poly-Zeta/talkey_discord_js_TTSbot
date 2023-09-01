f526be8e54836fcfc8d7563fcfb482ac58622788
Polyotu <55525642+Polyotu@users.noreply.github.com>
2023-09-01 10:54:58 +0900

    bugfix print文のせいだった

--- functions/llmMap.js
+++ functions/llmMap.js
@@ -16,12 +16,11 @@ async function addLlamaQueue(guildId, nickname, readTxt, uid,textChannel,botConn
 };
 
 async function processLlamaQueue(queue) {
-    console.log("loop");
+    // console.log("loop");
     // console.log(`llmqueue@processllamaqueue:${llmQueue}`);
     // console.log(`queue@processllamaqueue:${queue}`);
-    console.log(`queue.length@processllamaqueue:${queue.length}`);
+    // console.log(`queue.length@processllamaqueue:${queue.length}`);
     // console.log(`queue[0]@processllamaqueue:${queue[0]}`);
-    console.log(`queue[0].readTxt@1:${queue[0].readTxt}`);
     if (queue.length==0) {
         return;
     }else{
@@ -30,13 +29,14 @@ async function processLlamaQueue(queue) {
     
     //英訳
     queue[0].readTxt=await getResponseofTranslateAPI(queue[0].readTxt,"ja","en");
-    console.log(`queue[0].readTxt@1:${queue[0].readTxt}`);
+    // console.log(`queue[0].readTxt@1:${queue[0].readTxt}`);
 
     //llama
+    queue[0].readTxt=await getResponseofLlamaAPI(queue[0].nickname,queue[0].readTxt);
 
     //和訳
     queue[0].readTxt=await getResponseofTranslateAPI(queue[0].readTxt,"en","ja");
-    console.log(`queue[0].readTxt@1:${queue[0].readTxt}`);
+    // console.log(`queue[0].readTxt@1:${queue[0].readTxt}`);
 
     //音声再生にスタック<-botConnectionに応じた動作が必要かも
 
