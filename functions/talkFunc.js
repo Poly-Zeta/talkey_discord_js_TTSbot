57851434c868b1e9d44ff3325e76906e07981ebe
Polyotu <55525642+Polyotu@users.noreply.github.com>
2023-09-06 03:14:29 +0900

    ローカルllmに投げるよう変更

--- functions/talkFunc.js
+++ functions/talkFunc.js
@@ -35,27 +35,29 @@ async function talkFunc(readTxt, guildId, textChannel, botConnection, nickname,u
             const readreq = textOperator(readTxt);
             addAudioToMapQueue(guildId, nickname, readreq, "f2");
         }
-
+        
+        readTxt = readTxt.replace(namePattern, "talkey-chan");
+        await addLlamaQueue(guildId, nickname, readTxt, uid,textChannel,botConnection);
         // console.log(`namechk: ${readTxt}`);
-        var apiResponseText="";
-        const apiRandomizer = Math.floor(Math.random() * 100);
+        // var apiResponseText="";
+        // const apiRandomizer = Math.floor(Math.random() * 100);
 
-        if (apiRandomizer< 70) {
-            readTxt = readTxt.replace(namePattern, "talkeyちゃん");
-            apiResponseText = await getResponseofMebo(readTxt,nickname,uid);
-        }else{
-            console.log("a3rt");
-            readTxt = readTxt.replace(namePattern, "");
-            //a3rtに投げる
-            apiResponseText = await getResponseofTalkAPI(readTxt);
-        }
+        // if (apiRandomizer< 70) {
+        //     readTxt = readTxt.replace(namePattern, "talkeyちゃん");
+        //     apiResponseText = await getResponseofMebo(readTxt,nickname,uid);
+        // }else{
+        //     console.log("a3rt");
+        //     readTxt = readTxt.replace(namePattern, "");
+        //     //a3rtに投げる
+        //     apiResponseText = await getResponseofTalkAPI(readTxt);
+        // }
 
-        //ボイチャに接続している場合は応答をf1ボイスにしてqueueに投げる
-        if (botConnection != undefined) {
-            addAudioToMapQueue(guildId, "たーきーちゃん", apiResponseText, "f1");
-        }
-        //応答をreplyで返す
-        textChannel.send(apiResponseText);
+        // //ボイチャに接続している場合は応答をf1ボイスにしてqueueに投げる
+        // if (botConnection != undefined) {
+        //     addAudioToMapQueue(guildId, "たーきーちゃん", apiResponseText, "f1");
+        // }
+        // //応答をreplyで返す
+        // textChannel.send(apiResponseText);
     } else {
         // console.log("chk3");
         //名前が無ければ，ボイチャに接続しているかを確認して入力をそのままf1ボイスでqueueに追加
