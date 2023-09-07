5655d3d26c3c822e9a7b4b8b5ac50dd1e7f82c8b
Polyotu <55525642+Polyotu@users.noreply.github.com>
2023-09-07 09:56:00 +0900

    ELYZAに対応させてみる

--- functions/llmMap.js
+++ functions/llmMap.js
@@ -12,7 +12,8 @@ async function addLlamaQueue(guildId, nickname, readTxt, uid,textChannel,botConn
     llmQueue.push({guildId, nickname, readTxt, uid,textChannel,botConnection});
     if (startlength == 0) {
         // console.log(`llmqueue@addllamaqueue:${llmQueue}`);
-        processLlamaQueue(llmQueue);
+        // processLlamaQueue(llmQueue);
+        processELYZAQueue(llmQueue);
     }
     return;
 };
@@ -64,6 +65,40 @@ async function processLlamaQueue(queue) {
 
     queue.shift();
     processLlamaQueue(queue);
+};
+
+async function processELYZAQueue(queue) {
+    console.log("loop");
+    // console.log(`llmqueue@processllamaqueue:${llmQueue}`);
+    // console.log(`queue@processllamaqueue:${queue}`);
+    // console.log(`queue.length@processllamaqueue:${queue.length}`);
+    // console.log(`queue[0]@processllamaqueue:${queue[0]}`);
+    if (!queue[0]?.readTxt || queue.length==0) {
+        return;
+    }else{
+        console.log(queue.length);
+    }
+
+    const namePattern = /たーきーちゃん|ターキーちゃん|たーきーくん|ターキーくん/;
+    queue[0].readTxt = queue[0].readTxt.replace(namePattern, "talkeyちゃん");
+    
+    console.log(`入力:${queue[0].readTxt}`);
+
+    //llama
+    queue[0].textChannel.sendTyping();
+    queue[0].readTxt=await getResponseofLlamaAPI(queue[0].nickname,queue[0].readTxt);
+    console.log(`入力->llm:${queue[0].readTxt}`);
+
+    //音声再生にスタック
+    if (queue[0].botConnection != undefined) {
+        addAudioToMapQueue(queue[0].guildId, "たーきーちゃん", queue[0].readTxt.replace(/talkey/g, `たーきー`), "f1");
+    }
+
+    //該当テキストチャットにメッセージ送信
+    queue[0].textChannel.send(queue[0].readTxt);
+
+    queue.shift();
+    processELYZAQueue(queue);
 };
 
 module.exports = {
