21b90564de62efda5c74ddcf62dd06835f0c1f34
Polyotu <55525642+Polyotu@users.noreply.github.com>
2023-09-19 13:43:16 +0900

    オプションの形式変更

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,4 +1,5 @@
 const { execSync } = require('child_process');
+const { Console } = require('console');
 
 var fs = require('fs');
 var path = require('path');
@@ -28,21 +29,42 @@ module.exports = {
         description: "再起動",
         options: [
             {
-                type: 1,//"SUB_COMMAND",
-                name: "default",
-                description: "再起動",
-            },
-            {
-                type: 1,//"SUB_COMMAND",
-                name: "upgrade",
-                description: "gitから最新安定版をpull",
-            },
-            {
-                type: 1,//"SUB_COMMAND",
-                name: "develop",
-                description: "gitから開発版をpull",
-            },
-        ]
+                type: 3,//"STRING",
+                name: "branch",
+                description: "githubから引っ張ってくる元の選択",
+                choices: [
+                    {
+                        name: "upgrade",
+                        value: "master",
+                    },
+                    {
+                        name: "develop",
+                        value: "develop",
+                    },
+                    {
+                        name: "hotfix",
+                        value: "hotfix",
+                    },
+                ]
+            }
+        ],
+        // options: [
+        //     {
+        //         type: 1,//"SUB_COMMAND",
+        //         name: "default",
+        //         description: "再起動",
+        //     },
+        //     {
+        //         type: 1,//"SUB_COMMAND",
+        //         name: "upgrade",
+        //         description: "gitから最新安定版をpull",
+        //     },
+        //     {
+        //         type: 1,//"SUB_COMMAND",
+        //         name: "develop",
+        //         description: "gitから開発版をpull",
+        //     },
+        // ]
     },
     async execute(interaction) {
         // console.log(tokens.errorNotifyChannel);
@@ -50,13 +72,23 @@ module.exports = {
             // return interaction.reply("作者限定のコマンド");
             return interaction.editReply("作者限定のコマンド");
         }
-        const subCommand = interaction.options.getSubcommand(false);
-        console.log(subCommand);
+        const cmdOption=interaction.options.get("branch",false);
+        // const subCommand = interaction.options.getSubcommand(false);
+        // console.log(subCommand);
+        let branchOpt;
+        if(cmdOption!==null){
+            branchOpt=cmdOption.value;
+        }else{
+            branchOpt="default";
+        }
+        console.log(branchOpt);
 
         // await interaction.reply(`${subCommand} : 再起動します`);
-        await interaction.editReply(`${subCommand} : 再起動します`);
+        // await interaction.editReply(`${subCommand} : 再起動します`);
+        await interaction.editReply(`${branchOpt} : 再起動します`);
 
-        if (subCommand == "default") {
+        // if (subCommand == "default") {
+        if (branchOpt == "default") {
             statConfig.reboot += 1;
             //同期，上書き
             fs.writeFileSync(
@@ -74,16 +106,18 @@ module.exports = {
             "utf-8"
         );
 
-        var branchName;
-        if (subCommand == "upgrade") {
-            console.log("upgrade");
-            branchName="master";
-        }else if (subCommand == "develop") {
-            console.log("develop");
-            branchName="develop";
-        }
-        const stdout = execSync(`git fetch origin ${branchName}`);
-        const stdout2=execSync(`git reset --hard origin/${branchName}`);
+        // var branchName;
+        // if (subCommand == "upgrade") {
+        //     console.log("upgrade");
+        //     branchName="master";
+        // }else if (subCommand == "develop") {
+        //     console.log("develop");
+        //     branchName="develop";
+        // }
+        // const stdout = execSync(`git fetch origin ${branchName}`);
+        // const stdout2=execSync(`git reset --hard origin/${branchName}`);
+        const stdout = execSync(`git fetch origin ${branchOpt}`);
+        const stdout2=execSync(`git reset --hard origin/${branchOpt}`);
 
         return;
     }
