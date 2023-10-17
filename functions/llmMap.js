265690da1d8b23265a45ddb081541bf3f06a718f
Polyotu <55525642+Polyotu@users.noreply.github.com>
2023-10-18 04:58:47 +0900

    会話履歴保持への対応

--- functions/llmMap.js
+++ functions/llmMap.js
@@ -5,12 +5,38 @@ const { addAudioToMapQueue } = require('../functions/audioMap.js');
 //https://zenn.dev/s7/articles/86511eb5089fb6c05599
 //************************************************************************************************************************ */
 
+//会話履歴の保持
+//事前にMapを作っておく
+//key:llmQueue登録時のguildID，value:そこそこの件数を保持したリスト
+//llmqueueが動作するとき，キーから履歴を呼び出して直近n件をピックアップ
+//prompt末尾の会話履歴部分に挿入する
+//デフォルトの対話は各マップの項目作成時に挿入しておく
+
 const llmQueue = [];
-const talkMemoryLength=3;
+const talkMemoryLength=6;
+const talkMemoryMaxLength=30;
+const talkMemoryMap=new Map();
 
 async function addLlamaQueue(guildId, nickname, readTxt, uid,textChannel,botConnection,doMoldProcessFlg) {
     const startlength=llmQueue.length;
     llmQueue.push({guildId, nickname, readTxt, uid,textChannel,botConnection,doMoldProcessFlg});
+
+    //当該ギルドに会話履歴が無ければ作成
+    //デモ応答を登録する
+    if(!talkMemoryMap.has(guildID)){
+        talkMemoryMap.set(guildId,
+            //このデモ応答登録は後々ファイル読み出しにした方が良い
+            //そうなっていれば，ファイルIOにして記憶を引き継げる
+            [
+                "userinput:875oasfj,2023/2/17,19:57,お元気ですか？ \n ",
+                "あなたの回答:こんばんは！私は元気です！875oasfjさんはお元気ですか？; \n ",
+                "userinput:falken936,2022/12/28,21:40,今日は仕事に行きたくない \n ",
+                "あなたの回答:falken936さん、大丈夫ですか？休憩は取れていますか？; \n ",
+                "userinput:user556,2021/1/3,1:26,お喋りしてくれてありがとうターキーちゃん。私は今から皿を洗います。応援しててね！ \n ",
+                "あなたの回答:user556さん、こちらこそありがとうございました。頑張ってくださいね！; \n ",
+            ]
+        );
+    }
     if (startlength == 0) {
         // console.log(`llmqueue@addllamaqueue:${llmQueue}`);
         // processLlamaQueue(llmQueue);
@@ -87,6 +113,21 @@ async function processELYZAQueue(queue) {
 
     //llama
     queue[0].textChannel.sendTyping();
+
+    //prompt末尾にここまでの会話ログ(nラリー未満の場合はデモ応答を含む)を付けたす
+    //応答ログに最新の入力を登録，ログからn+1(<-ここまでのn/2ラリー+今登録した1件)個の応答を引き出す
+    //最新のユーザ入力の成形
+    const date=new Date;
+    const dateAndTime=`${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()},${date.getHours()}:${date.getMinutes()}`;
+    queue[0].readTxt=`userinput:${queue[0].nickname},${dateAndTime},${queue[0].readTxt} \n `;
+    //最新のユーザ入力をログに登録
+    const guildLog=talkMemoryMap.get(queue[0].guildID);
+    guildLog.push(queue[0].readTxt);
+    //ログの長さは一定で切る
+    if(guildLog.length>talkMemoryMaxLength){guildLog.shift();}
+    //ログからn/2回の会話往復と最新のユーザ入力を引き出して成形
+    queue[0].readTxt=guildLog.slice(0,talkMemoryLength+1).join("");
+
     queue[0].readTxt=await getResponseofLlamaAPI(queue[0].nickname,queue[0].readTxt);
     if(queue[0].doMoldProcessFlg){
         const tmp=queue[0].readTxt.split(';');
@@ -103,6 +144,11 @@ async function processELYZAQueue(queue) {
 
     //該当テキストチャットにメッセージ送信
     queue[0].textChannel.send(queue[0].readTxt);
+
+    //llmの生成した応答をログに保存
+    guildLog.push(queue[0].readTxt);
+    if(guildLog.length>talkMemoryMaxLength){guildLog.shift();}
+    console.log(guildLog.slice(0,3));
 
     queue.shift();
     processELYZAQueue(queue);
