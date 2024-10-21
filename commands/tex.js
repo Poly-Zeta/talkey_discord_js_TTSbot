8dc4af41f1e0f4cabd62859c8bc1fa8dab753412
Polyotu <55525642+Polyotu@users.noreply.github.com>
2024-10-22 04:34:01 +0900

    画像の編集6

--- commands/tex.js
+++ commands/tex.js
@@ -30,29 +30,45 @@ module.exports = {
                 .replace(/<\/mjx-container>/, "");
         });
 
-        const metadata = await sharp(Buffer.from(svgStr)).metadata();
+        // const metadata = await sharp(Buffer.from(svgStr)).metadata();
         // const resizeWidth = metadata.width>600 ? metadata.width:600;
         // const resizeHeight = metadata.height>400 ? metadata.height:400;
-        const pngBuff = await sharp(Buffer.from(svgStr))
-        // .negate()
-        .bandbool("and")
-        .ensureAlpha()
+        
+        let cBuf=Buffer.from(svgStr);
+        //2値化->背景ffffff,文字000000，余白も付けておく
+        cBuf=await sharp(cBuf)
+        .extend({
+            top: 10,
+            right: 10,
+            bottom: 10,
+            left: 10,
+            background: '#fff'
+        })
+        .threshold(150)
+        .toBuffer();
+        //反転->背景000000,文字ffffff
+        cBuf=await sharp(cBuf).negate().toBuffer();
+        //αを追加
+        cBuf=await sharp(cBuf).ensureAlpha().toBuffer();
+        //全chの値を非αの値に指定->文字のffffffとαを共通にして，残りは0で透過
+        cBuf=await sharp(cBuf).extractChannel("red").toBuffer();
+        //リサイズして終了
+        const pngBuff = await sharp(cBuf)
         .resize({
             width: 500,
             height: 300,
             fit: 'outside'
         })
         .toBuffer();
-        // const pngBuff = await sharp({ 
-        //     create: { 
-        //         width: metadata.width, 
-        //         height: metadata.height,
-        //         channels: 4, 
-        //         background: { r: 255, g: 255, b: 255, alpha: 0 }}
-        // })
-        // .composite(Buffer.from(svgStr))
+        // const pngBuff = await sharp(Buffer.from(svgStr))
         // .negate()
-        // // .resize(resizeWidth,resizeHeight)
+        // .bandbool("and")
+        // .ensureAlpha()
+        // .resize({
+        //     width: 500,
+        //     height: 300,
+        //     fit: 'outside'
+        // })
         // .toBuffer();
 
         const attachment = new AttachmentBuilder()
