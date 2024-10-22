1bb942f605484c25f3d49cc4d0baeb314b46b9f3
Polyotu <55525642+Polyotu@users.noreply.github.com>
2024-10-22 21:43:52 +0900

    try位置変更

--- commands/tex.js
+++ commands/tex.js
@@ -21,73 +21,90 @@ module.exports = {
         let reply = `> ${readTxt}`;
         let svgStr="";
 
-        await mathjax.init({
-            loader: {load: ["input/tex", "output/svg"]}
-        }).then(async (MathJax) => {
-            try{
-                const svg = await MathJax.tex2svg(` ${readTxt}`, {display: true});
-                svgStr = await MathJax.startup.adaptor.outerHTML(svg)
-                    .replace(/<mjx-container class="MathJax" jax="SVG" display="true">/, "")
-                    .replace(/<\/mjx-container>/, "");
-            }catch(e){
-                return await interaction.editReply("対応していない数式が入力されました．エラーのため，コマンド実行を中断します．");
+        const MathJax=await mathjax.init(
+            {
+                loader: {
+                    load: ["input/tex", "output/svg"]
+                }
             }
-        });
+        );
 
-        // const metadata = await sharp(Buffer.from(svgStr)).metadata();
-        // const resizeWidth = metadata.width>600 ? metadata.width:600;
-        // const resizeHeight = metadata.height>400 ? metadata.height:400;
-        
-        let cBuf=Buffer.from(svgStr);
-        //2値化->背景ffffff,文字000000，α削除
-        // cBuf=await sharp(cBuf)
-        // .extend({
-        //     top: 3,
-        //     right: 3,
-        //     bottom: 3,
-        //     left: 3,
-        //     background: '#ffffff'
-        // })
-        // .threshold(50)
-        // .removeAlpha()
-        // .toBuffer();
+        try{
+            const svg = await MathJax.tex2svg(` ${readTxt}`, {display: true});
+            svgStr = await MathJax.startup.adaptor.outerHTML(svg)
+                .replace(/<mjx-container class="MathJax" jax="SVG" display="true">/, "")
+                .replace(/<\/mjx-container>/, "");
+                
+            // await mathjax.init({
+            //     loader: {load: ["input/tex", "output/svg"]}
+            // }).then(async (MathJax) => {
+            //     try{
+            //         const svg = await MathJax.tex2svg(` ${readTxt}`, {display: true});
+            //         svgStr = await MathJax.startup.adaptor.outerHTML(svg)
+            //             .replace(/<mjx-container class="MathJax" jax="SVG" display="true">/, "")
+            //             .replace(/<\/mjx-container>/, "");
+            //     }catch(e){
+            //         return await interaction.editReply("対応していない数式が入力されました．エラーのため，コマンド実行を中断します．");
+            //     }
+            // });
 
-        // cBuf=await sharp(cBuf).extractChannel("alpha").toBuffer();
+            // const metadata = await sharp(Buffer.from(svgStr)).metadata();
+            // const resizeWidth = metadata.width>600 ? metadata.width:600;
+            // const resizeHeight = metadata.height>400 ? metadata.height:400;
+            
+            let cBuf=Buffer.from(svgStr);
+            //2値化->背景ffffff,文字000000，α削除
+            // cBuf=await sharp(cBuf)
+            // .extend({
+            //     top: 3,
+            //     right: 3,
+            //     bottom: 3,
+            //     left: 3,
+            //     background: '#ffffff'
+            // })
+            // .threshold(50)
+            // .removeAlpha()
+            // .toBuffer();
 
-        //反転->背景000000,文字ffffff
-        // cBuf=await sharp(cBuf).negate().toBuffer();
+            // cBuf=await sharp(cBuf).extractChannel("alpha").toBuffer();
 
-        // cBuf=await sharp(cBuf).removeAlpha().toBuffer();
+            //反転->背景000000,文字ffffff
+            // cBuf=await sharp(cBuf).negate().toBuffer();
 
-        //αを追加
-        // cBuf=await sharp(cBuf).ensureAlpha().toBuffer();
+            // cBuf=await sharp(cBuf).removeAlpha().toBuffer();
 
-        //全chの値を非αの値に指定->文字のffffffとαを共通にして，残りは0で透過
-        // cBuf=await sharp(cBuf).extractChannel("red").toBuffer();
+            //αを追加
+            // cBuf=await sharp(cBuf).ensureAlpha().toBuffer();
 
-        //リサイズして終了
-        const pngBuff = await sharp(cBuf)
-        .resize({
-            width: 500,
-            height: 300,
-            fit: 'outside'
-        })
-        .toBuffer();
-        // const pngBuff = await sharp(Buffer.from(svgStr))
-        // .negate()
-        // .bandbool("and")
-        // .ensureAlpha()
-        // .resize({
-        //     width: 500,
-        //     height: 300,
-        //     fit: 'outside'
-        // })
-        // .toBuffer();
+            //全chの値を非αの値に指定->文字のffffffとαを共通にして，残りは0で透過
+            // cBuf=await sharp(cBuf).extractChannel("red").toBuffer();
 
-        const attachment = new AttachmentBuilder()
-            .setFile(pngBuff)
-        
-        await interaction.editReply(reply);
-        return await interaction.editReply({ files: [attachment]});
+            //リサイズして終了
+            const pngBuff = await sharp(cBuf)
+            .resize({
+                width: 500,
+                height: 300,
+                fit: 'outside'
+            })
+            .toBuffer();
+            // const pngBuff = await sharp(Buffer.from(svgStr))
+            // .negate()
+            // .bandbool("and")
+            // .ensureAlpha()
+            // .resize({
+            //     width: 500,
+            //     height: 300,
+            //     fit: 'outside'
+            // })
+            // .toBuffer();
+
+            const attachment = new AttachmentBuilder()
+                .setFile(pngBuff)
+            
+            await interaction.editReply(reply);
+            return await interaction.editReply({ files: [attachment]});
+        }catch(e){
+            return await interaction.editReply("対応していない数式が入力されました．エラーのため，コマンド実行を中断します．");
+        }
     }
 }
